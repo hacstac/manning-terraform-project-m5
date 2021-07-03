4544da6fecbfd4e1bfff4e6f2db0510836011237
Akash Rajvanshi <Inputhash@gmail.com>
2021-07-03 15:22:39 +0530

    Added: Create 100 Users with TF

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "registry.terraform.io/hashicorp/aws"
+      version = "3.48.0"
+    }
+  }
+}
+
+// instantiate AWS provider with a region
+provider "aws" {
+  region = "ap-south-1"
+}
+
+// create 100 users
+module "users" {
+  source = "./modules/cloudesk-user/"
+  for_each = toset([
+    for i in range(100) : format("user-%02d", i)
+  ])
+  name = each.key
+}
